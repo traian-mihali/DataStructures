0a9812129b6dac7778c1af9d09b10c6a6ec8fc25
Traian Mihali <traianmihali696@gmail.com>
2020-10-25 17:29:08 +0200

    Add Array implementation

--- LinearStructures/Array.cs
+++ LinearStructures/Array.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,112 @@ namespace DataStructures.LinearStructures
 {
     public class Array
     {
+        private int[] _items = null;
+        private int _count = 0;
 
+        public Array(int size)
+        {
+            _items = new int[size];
+        }
+
+
+        public void Insert(int item)
+        {
+            Resize();
+
+            _items[_count++] = item;
+        }
+
+        public void InsertAt(int index, int item)
+        {
+            if (index < 0 || index > _count)
+                throw new ArgumentException();
+
+            Resize();
+
+            for (var i = _count - 1; i >= index; i--)
+                _items[i + 1] = _items[i];
+
+            _items[index] = item;
+
+            _count++;
+        }
+
+        public int IndexOf(int item)
+        {
+            for (var i = 0; i < _count; i++)
+                if (_items[i] == item)
+                    return i;
+
+            return -1;
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentException();
+
+            var deleted = _items[index];
+
+            for (var i = index; i < _count - 1; i++)
+                _items[i] = _items[i + 1];
+
+            _count--;
+
+            return deleted;
+        }
+
+        public int[] Reverse()
+        {
+            int[] newItems = new int[_count];
+
+            for (var i = 0; i < _count; i++)
+                newItems[i] = _items[_count - i - 1];
+
+            return newItems;
+        }
+
+        public int Max()
+        {
+            var max = 0;
+
+            foreach (var item in _items)
+            {
+                if (item > max)
+                    max = item;
+            }
+
+            return max;
+        }
+
+        public Array Intersect(Array other)
+        {
+            var intersection = new Array(_count);
+
+            foreach (var item in _items)
+                if (other.IndexOf(item) >= 0)
+                    intersection.Insert(item);
+
+            return intersection;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < _count; i++)
+                Console.WriteLine(_items[i]);
+        }
+
+        private void Resize()
+        {
+            if (_items.Length == _count)
+            {
+                int[] newItems = new int[_count * 2];
+
+                for (int i = 0; i < _count; i++)
+                    newItems[i] = _items[i];
+
+                _items = newItems;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,42 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST");
+            var array = new LinearStructures.Array(3);
+
+            array.Insert(1);
+            array.Insert(2);
+            array.Insert(3);
+
+            array.Print();
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("REMOVE FROM INDEX 0");
+            var deleted = array.RemoveAt(0);
+            Console.WriteLine("ITEM REMOVED: " + deleted);
+            array.Print();
+
+            Console.WriteLine("INSERT 1 AT INDEX 0");
+            array.InsertAt(0, 1);
+            array.Print();
+
+            var reversed = array.Reverse();
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("REVERSED ARRAY");
+            foreach (var item in reversed)
+                Console.WriteLine(item);
+
+
+            Console.WriteLine(Environment.NewLine);
+
+            var other = new LinearStructures.Array(2);
+            other.Insert(1);
+            other.Insert(4);
+
+            Console.WriteLine("INTERSECTION WITH [1, 4]");
+            var intersection = array.Intersect(other);
+            intersection.Print();
+
             Console.ReadLine();
         }
     }
