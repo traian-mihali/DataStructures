af722469a911555bce3ab014e4b3ec0cf9666147
Traian Mihali <traianmihali696@gmail.com>
2020-10-26 23:40:25 +0200

    Add LinkedList implementation

---
+++ LinearStructures/LinkedList.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinearStructures
+{
+    public class LinkedList
+    {
+        private Node First { get; set; }
+        private Node Last { get; set; }
+        public int Size { get; private set; }
+
+        public void AddFirst(int value)
+        {
+            var node = new Node(value);
+
+            if (IsEmpty())
+                First = Last = node;
+            else
+            {
+                node.Next = First;
+                First = node;
+            }
+
+            Size++;
+        }
+
+        public void AddLast(int value)
+        {
+            var node = new Node(value);
+
+            if (IsEmpty())
+                First = Last = node;
+            else
+            {
+                Last.Next = node;
+                Last = node;
+            }
+
+            Size++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The LinkedList is empty.");
+
+            if (First == Last)
+            {
+                First = Last = null;
+            }
+            else
+            {
+                var second = First.Next;
+                First.Next = null;
+                First = second;
+            }
+
+            Size--;
+        }
+
+        public void RemoveLast()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The LinkedList is empty.");
+
+            if (First == Last)
+            {
+                First = Last = null;
+            }
+            else
+            {
+                var previous = GetPrevious(Last);
+                previous.Next = null;
+                Last = previous;
+            }
+
+            Size--;
+        }
+
+        public int IndexOf(int value)
+        {
+            var index = 0;
+            var current = First;
+
+            while(current != null)
+            {
+                if (current.Value == value)
+                    return index;
+
+                current = current.Next;
+                index++;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(int value)
+        {
+            return IndexOf(value) != -1;
+        }
+
+        public void Reverse()
+        {
+            if (IsEmpty())
+                return;
+
+            var current = First;
+            var next = First.Next;
+
+            while (next != null)
+            {
+                var temp = next.Next;
+                next.Next = current;
+
+                current = next;
+                next = temp;
+            }
+
+            Last = First;
+            Last.Next = null;
+
+            First = current;
+        }
+
+        public int GetKthFromTheEnd(int k)
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The LinkedList Is Empty.");
+
+            if (k < 1 || k > Size)
+                throw new InvalidOperationException("Invalid k argument.");
+
+            var target = First;
+            var pointer = First;
+
+            for (var i = 0; i < k - 1; i++)
+                pointer = pointer.Next;
+
+            while (pointer.Next != null)
+            {
+                pointer = pointer.Next;
+                target = target.Next;
+            }
+
+            return target.Value;
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Size];
+
+            var current = First;
+            var index = 0;
+            while(current != null)
+            {
+                array[index++] = current.Value;
+                current = current.Next;
+            }
+
+            return array;
+        }
+
+        public static LinkedList CreateLoop()
+        {
+            var list = new LinkedList();
+
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+
+            var node = list.Last;
+
+            list.AddLast(40);
+            list.AddLast(50);
+            list.AddLast(60);
+
+            list.Last.Next = node;
+
+            return list;
+        }
+
+        public bool HasLoop()
+        {
+            var slow = First;
+            var fast = First;
+
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (slow == fast)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void PrintMiddle()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The LinkedList Is Empty.");
+
+            var target = First;
+            var pointer = First;
+
+            while (pointer != Last && pointer.Next != Last)
+            {
+                pointer = pointer.Next.Next;
+                target = target.Next;
+            }
+
+            if (pointer == Last)
+                Console.WriteLine(target.Value);
+            else
+                Console.WriteLine(target.Value + ", " + target.Next.Value);
+        }
+
+        public void Print()
+        {
+            var current = First;
+            while (current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Next;
+            }
+        }
+
+        private Node GetPrevious(Node node)
+        {
+            var current = First;
+
+            while (current != null)
+            {
+                if (current.Next == node)
+                    return current;
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        private bool IsEmpty()
+        {
+            return First == null;
+        }
+
+        private class Node
+        {
+            internal int Value { get; set; }
+            internal Node Next { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures.LinearStructures;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,43 +12,114 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            //TestArray();
+            TestLinkedList();
+
+            Console.ReadLine();
+        }
+
+        static void TestArray()
+        {
             var array = new LinearStructures.Array(3);
 
             array.Insert(1);
             array.Insert(2);
             array.Insert(3);
-
             array.Print();
-            Console.WriteLine(Environment.NewLine);
 
+
+            Console.WriteLine(Environment.NewLine);
             Console.WriteLine("REMOVE FROM INDEX 0");
-            var deleted = array.RemoveAt(0);
-            Console.WriteLine("ITEM REMOVED: " + deleted);
+            array.RemoveAt(0);
             array.Print();
 
             Console.WriteLine("INSERT 1 AT INDEX 0");
             array.InsertAt(0, 1);
             array.Print();
 
-            var reversed = array.Reverse();
-
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("REVERSED ARRAY");
+            var reversed = array.Reverse();
             foreach (var item in reversed)
                 Console.WriteLine(item);
 
 
             Console.WriteLine(Environment.NewLine);
-
+            Console.WriteLine("INTERSECTION WITH [1, 4]");
             var other = new LinearStructures.Array(2);
             other.Insert(1);
             other.Insert(4);
-
-            Console.WriteLine("INTERSECTION WITH [1, 4]");
             var intersection = array.Intersect(other);
             intersection.Print();
+        }
 
-            Console.ReadLine();
+        static void TestLinkedList()
+        {
+            var list = new LinkedList();
+
+            //list.AddFirst(10);
+            //list.AddFirst(20);
+            //list.AddFirst(30);
+            //list.Print();
+
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            list.AddLast(40);
+            list.AddLast(50);
+            list.Print();
+
+            var loopList = LinkedList.CreateLoop();
+            Console.WriteLine("HAS LOOP: " + loopList.HasLoop());
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("LIST MIDDLE VALUE: ");
+            list.PrintMiddle();
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("kth is 1: " + list.GetKthFromTheEnd(1));
+            Console.WriteLine("kth is 2: " + list.GetKthFromTheEnd(2));
+            Console.WriteLine("kth is 3: " + list.GetKthFromTheEnd(3));
+
+
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("LINKEDLIST SIZE: " + list.Size);
+
+            //Console.WriteLine(Environment.NewLine);
+            //list.RemoveFirst();
+            //list.Print();
+
+            //Console.WriteLine(Environment.NewLine);
+            //list.RemoveLast();
+            //list.Print();
+
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("INDEX OF 10: " + list.IndexOf(10));
+            Console.WriteLine("INDEX OF 20: " + list.IndexOf(20));
+            Console.WriteLine("INDEX OF 30: " + list.IndexOf(30));
+            Console.WriteLine("INDEX OF 40: " + list.IndexOf(40));
+
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("CONTAINS 10: " + list.Contains(10));
+            Console.WriteLine("CONTAINS 20: " + list.Contains(20));
+            Console.WriteLine("CONTAINS 30: " + list.Contains(30));
+            Console.WriteLine("CONTAINS 40: " + list.Contains(40));
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("LINKEDLIST SIZE: " + list.Size);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("LINKEDLIST TO ARRAY: " + String.Join(", ", list.ToArray()));
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("REVERSE LINKEDLIST");
+            list.Reverse();
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("LINKEDLIST TO ARRAY: " + String.Join(", ", list.ToArray()));
         }
     }
 }
